4752be4d235e8f3f19018d0f3e472c3857fc9482
Luís Pacheco <30757322+xaviluisxavier@users.noreply.github.com>
2026-03-09 16:19:52 -0100

    Clean up comments in form submission handler
    Removed comments for value capture and validation.

--- external-scripts.js
+++ external-scripts.js
@@ -5,11 +5,9 @@ const campos = ['name', 'email', 'subject', 'message']
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     
-    // Captura de valores
     const email = document.getElementById('email').value;
     const nome = document.getElementById('name').value;    
 
-    // Validação simples 
     if (!email.includes('@uac.pt') && !email.includes('@gmail.com')) {
         alert("Por favor, use um email válido."); 
         return;
