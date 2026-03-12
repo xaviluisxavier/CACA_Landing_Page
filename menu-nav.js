0bd2fab14a9d23302f280e8925fb8b95ba71b282
Luís Pacheco <30757322+xaviluisxavier@users.noreply.github.com>
2026-03-12 09:58:50 -0100

    Delete menu-nav.js

--- menu-nav.js
+++
@@ -1,8 +0,0 @@
-// Seleção de elementos do DOM para o menu mobile
-const menuToggle = document.getElementById('menuToggle');
-const mainNav = document.getElementById('mainNav');
-
-// Adiciona o evento de clique para alternar a visibilidade do menu em dispositivos móveis
-menuToggle.addEventListener('click', () => {
-    mainNav.classList.toggle('active');
-});
