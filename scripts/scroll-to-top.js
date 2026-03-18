b7b409de480fe19d2b49d46f9d10d8d39390601d
Luís Pacheco <30757322+xaviluisxavier@users.noreply.github.com>
2026-03-18 16:14:06 -0100

    Update scroll-to-top.js

--- scripts/scroll-to-top.js
+++ scripts/scroll-to-top.js
@@ -1,23 +1,13 @@
-// Seleção das áreas afetadas
-const btnTopo = document.getElementById("btn-topo");
+export function initScrollToTop() {
+    const btnTopo = document.getElementById("btn-topo");
 
-/**
- * Função para regressar ao topo da página suavemente
- * @param {Event} e Objeto de evento injetado pelo browser
- */function scroll_to_top(e) {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-}
+    if (btnTopo) {
+        window.addEventListener("scroll", () => {
+            btnTopo.style.display = window.scrollY > 300 ? "block" : "none";
+        });
 
-/**
- * Função para mostrar o botão apenas se o utilizador fizer scroll para baixo (> 300px)
- */ 
-function show_btn() {
-    if (window.scrollY > 300) {
-        btnTopo.style.display = "block";
-    } else {
-        btnTopo.style.display = "none";
+        btnTopo.addEventListener("click", () => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        });
     }
 }
-
-window.addEventListener("scroll", show_btn);
-btnTopo.addEventListener("click", scroll_to_top);
